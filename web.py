0b9db5b33aadd63ddca8954c5bd3283090f4c309
eli-suarez845 <elisasuarezmoreira@gmail.com>
2023-07-11 22:12:00 -0300

    Implement delete method when a checkbox is checked

--- web.py
+++ web.py
@@ -14,10 +14,14 @@ st.title("My Todo App")
 st.subheader("This is my first app:")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)  # removes checked todo
+        functions.write_todos(todos)  #then re-write the function in todos
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Enter a new todo",
               on_change=add_todo, key='new_todo')  # on_change is like a custom function
 
-st.session_state
